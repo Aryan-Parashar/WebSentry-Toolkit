8d6c22a7a39cae3a2cdaeb556004af5aabee4257
Aryan Parashar <160344522+Aryan-Parashar@users.noreply.github.com>
2024-05-29 11:32:03 +0530

    Update BruteForce_done.py

--- BruteForce_done.py
+++ BruteForce_done.py
@@ -1,14 +1,6 @@
 import random
 import string
-import streamlit as st
 import requests
-
-# Color constants
-RED = '\033[0;31m'
-GREEN = '\033[0;32m'
-YELLOW = '\033[1;33m'
-BLUE = '\033[0;34m'
-NC = '\033[0m'  # No Color
 
 # Function to generate a random code with specified length and characters
 def generate_code(length, characters):
